53cc57db7c31fadf296e3f00dde1309c52214b91
Anna.Paramonycheva <anka.paramonycheva@yandex.ru>
2023-09-28 22:30:12 +0300

    день программиста

---
+++ src/Praktikum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            // здесь выводим результат
+            System.out.println ("12.09." + year);
+        } else {
+            System.out.println("13.09." + year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        // здесь определяем, является ли переданный год високосным
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
